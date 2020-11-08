6c4e878b2316f5e4075d959080b7228fe16d97b1
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-11-08 12:09:15 -0300

    Add EquiLeader solution

--- codility/lesson08/EquiLeader.java
+++ codility/lesson08/EquiLeader.java
@@ -1,5 +1,11 @@
 package codility.lesson08;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * A non-empty array A consisting of N integers is given.
  * 
@@ -38,4 +44,59 @@ package codility.lesson08;
  */
 public class EquiLeader {
 
+    private int countMax = 0;
+    Map<Integer, Integer> map;
+    Integer leader;
+
+    private Integer update(Integer currentElement, int numberElements) {
+        int count = map.getOrDefault(currentElement, 0) + 1;
+        map.put(currentElement, count);
+
+        if (count > countMax) {
+            countMax = count;
+            leader = currentElement;
+        }
+        if (countMax < ((numberElements / 2) + 1)) {
+            return null;
+        }
+        return leader;
+    }
+
+    public int solution(int[] A) {
+        int size = A.length;
+
+        // Calculate leaders from left to right:
+        Integer[] fromLeftArray = new Integer[size];
+        map = new HashMap<>();
+        countMax = 0;
+        for (int i = 0; i < size; i++) {
+            fromLeftArray[i] = update(A[i], i + 1);
+        }
+
+        // Calculate leaders from right to left:
+        Integer[] fromRightArray = new Integer[size];
+        map = new HashMap<>();
+        countMax = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            fromRightArray[i] = update(A[i], size - i);
+        }
+
+        // Calculate equi leader that are equal:
+        int count = 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (fromLeftArray[i] != null && fromRightArray[i + 1] != null) {
+                if (fromLeftArray[i].equals(fromRightArray[i + 1]))
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(2 == new EquiLeader().solution(new int[] { 4, 3, 4, 4, 4, 2 }));
+        System.out.println(1 == new EquiLeader().solution(new int[] { 4, 4 }));
+        System.out.println(0 == new EquiLeader().solution(new int[] { 4, 3 }));
+        System.out.println(0 == new EquiLeader().solution(new int[] { 3, 4 }));
+        System.out.println(1 == new EquiLeader().solution(new int[] { Integer.MAX_VALUE, Integer.MAX_VALUE }));
+    }
 }
