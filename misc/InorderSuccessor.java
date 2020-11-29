3d9463f62c24b21b85ecc445732eb56a2291a55d
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-11-29 11:59:40 -0300

    Add iterative InorderSuccessor solution

--- misc/InorderSuccessor.java
+++ misc/InorderSuccessor.java
@@ -30,7 +30,38 @@ public class InorderSuccessor {
     static class BinarySearchTree {
         Node root;
 
+        Node getLeftMost(Node node) {
+            Node currentNode = node;
+            while (currentNode.left != null)
+                currentNode = currentNode.left;
+            return currentNode;
+        }
+
+        Node getClosestParent(Node node, Node inputNode) {
+            Node currentNode = node;
+            while (currentNode != null) {
+                if (currentNode.key > inputNode.key)
+                    return currentNode;
+                currentNode = currentNode.parent;
+            }
+            return null;
+        }
+
         Node findInOrderSuccessor(Node inputNode) {
+            Node currentNode = root;
+            while (currentNode != null) {
+                if (currentNode.key == inputNode.key) {
+                    if (currentNode.right != null) {
+                        return getLeftMost(currentNode.right);
+                    } else {
+                        return getClosestParent(currentNode.parent, inputNode);
+                    }
+                } else if (inputNode.key < currentNode.key) {
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode = currentNode.right;
+                }
+            }
             return null;
         }
 
@@ -83,7 +114,6 @@ public class InorderSuccessor {
     }
 
     public static void main(String[] args) {
-        Node test = null, succ = null;
 
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(20);
@@ -94,16 +124,25 @@ public class InorderSuccessor {
         tree.insert(11);
         tree.insert(14);
 
-        test = tree.getNodeByKey(9);
+        test(tree, 9);
+        test(tree, 12);
+        test(tree, 14);
+        test(tree, 11);
+        test(tree, 5);
+        test(tree, 25);
+        test(tree, 20);
 
+    }
+
+    private static void test(BinarySearchTree tree, int key) {
+        Node test = null, succ = null;
+        test = tree.getNodeByKey(key);
         succ = tree.findInOrderSuccessor(test);
-
         if (succ != null) {
             System.out.println("Inorder successor of " + test.key + " is " + succ.key);
         } else {
-            System.out.println("Inorder successor does not exist");
+            System.out.println("Inorder successor of " + test.key + " does not exist");
         }
-
     }
 
 }
